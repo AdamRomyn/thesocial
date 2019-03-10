0cc67922dc844d09079b4338752c8058538fc14f
Adam Romyn <adamromyn12345@gmail.com>
2019-03-10 03:35:47 +0200

    added friends table and also the friends code in the user object

--- app/User.php
+++ app/User.php
@@ -36,4 +36,28 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // User friends
+
+    public function friends()
+    {
+        $friends = $this->belongsToMany('App\User', 'user_friends', 'user_id', 'friend_id');
+        return $friends;
+    }
+
+    public function addFriend($friend_id)
+    {
+        // Adds the friend and then also goes and adds this user to its friend's, friends list.
+        $this->friends()->attach($friend_id);   
+        $friend = User::find($friend_id);       
+        $friend->friends()->attach($this->id);  
+    }
+
+    public function removeFriend($friend_id)
+    {
+        // Removes the friend and the reference on the friend object to this object.
+        $this->friends()->detach($friend_id);   
+        $friend = User::find($friend_id);       
+        $friend->friends()->detach($this->id);  
+    }
 }
